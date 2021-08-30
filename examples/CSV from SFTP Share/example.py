323f98351501a162ac4d501c7845eac137a044ca
s8070370 <stefan.hoppe@tu-dresden.de>
2021-08-30 13:46:07 +0200

    new infos about the sensortypes needed for data interpretation

--- examples/CSV from SFTP Share/example.py
+++ examples/CSV from SFTP Share/example.py
@@ -3,6 +3,7 @@ import pandas as pd
 from dotenv import load_dotenv
 import os
 import re
+import sys
 from gewv_timeseries_client import TimeseriesClient
 from datetime import datetime
 from pytz import UTC
@@ -10,10 +11,20 @@ from time import sleep
 
 load_dotenv()
 
+DIR_PATH = os.path.dirname(__file__)
+print(DIR_PATH)
+#LOG_FILE = os.path.basename(__file__)
+#print(LOG_FILE)
+LOG_FILE = os.path.splitext(os.path.basename(__file__))[0]
+print(LOG_FILE)
+
 TARGET_BUCKET = os.getenv("TARGET_BUCKET") or "EXPERIMENTAL"
 
-RE_STATION_ID = r"station\d{10}"
-RE_CSV = r"MUC500_ID_\w{12}_TS_\d{10}.csv"
+RE_STATION_ID = re.compile(r"station(\d{2})")
+RE_CSV = re.compile(r"MUC500_ID_(\w{12})_TS_(\d{10}).csv")
+RE_DEVICE_ID_LUG = re.compile(r"LUG-*")
+RE_DEVICE_ID_EFE = re.compile(r"EFE-*")
+RE_DEVICE_ID_NZR = re.compile(r"NZR-*")
 INDEX = "Timestamp"
 
 # ignore the file from mac os that created in the background
@@ -30,10 +41,57 @@ COLLUMNS = set(
         "DeviceId",
         "Value0",
         "Unit0",
-        'Scale0',
-        "Description0",
+        "Scale0",
+        "Description0",        
     ]
 )
+
+""" COLLUMNS = set(
+    [
+        INDEX,
+        "DeviceId",
+        "Value0",
+        "Unit0",
+        "Scale0",
+        "Description0",
+        "Value1",
+        "Unit1",
+        "Scale1",
+        "Description1",
+        "Value2",
+        "Unit2",
+        "Scale2",
+        "Description2",
+        "Value3",
+        "Unit3",
+        "Scale3",
+        "Description3",
+        "Value4",
+        "Unit4",
+        "Scale4",
+        "Description4",
+        "Value5",
+        "Unit5",
+        "Scale5",
+        "Description5",
+        "Value6",
+        "Unit6",
+        "Scale6",
+        "Description6",
+        "Value7",
+        "Unit7",
+        "Scale7",
+        "Description7",
+        "Value8",
+        "Unit8",
+        "Scale8",
+        "Description8",
+        "Value9",
+        "Unit9",
+        "Scale9",
+        "Description9",
+    ]
+) """
         
 # this function parse the dates in the csv
 def DATE_PARSER(date_string: str):
@@ -58,7 +116,7 @@ my_fs = SSHFS(
 )
 
 client = TimeseriesClient.from_env_properties()
-client.create_bucket(TARGET_BUCKET)
+# client.create_bucket(TARGET_BUCKET) # geht nicht überall, da nur Admins die Berechtigung besitzen, Buckets zu erstellen
 
 # **********************************************************
 #   migration script
@@ -75,95 +133,204 @@ def migrate_csv(path: str, station_name: str, csv_name: str):
                 date_parser=DATE_PARSER,
                 usecols=lambda c: c in COLLUMNS,
             )
+        except OSError as err:
+            NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(f"{str(NOW)}: OS error: {0}".format(err))
+            logfile.write(f"{str(NOW)}: OS error: {0}".format(err)) 
+            return        
         except ValueError:
-            print(f"Failed to parse the file {path}. Skip this csv.")
+            NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(f"{str(NOW)}: Failed to parse the file {path}. Skip this csv.")
+            logfile.write(f"{str(NOW)}: Failed to parse the file {path}. Skip this csv.\n") 
             return
         except KeyError:
-            print(f"Failed to parse the file {path}. Skip this csv.")
+            NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(f"{str(NOW)}: Failed to parse the file {path}. Skip this csv.")
+            logfile.write(f"{str(NOW)}: Failed to parse the file {path}. Skip this csv.\n")
+            return
+        except:
+            NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(f"{str(NOW)}: Unexpected error:", sys.exc_info()[0])
+            logfile.write(f"{str(NOW)}: Unexpected error:", sys.exc_info()[0])
             return
 
-        logfile = open(f"D:\\timeseries-migrations\\examples\\CSV from SFTP Share\\station12_in_InfluxDB.txt",'r+')
-        text = logfile.read().splitlines()
-        if csv_name in text:
-            print(f"Upload  of {csv_name} is already done. Skip this csv.")
+        try:
+            #uploaded_files = open(f"D:\\timeseries-migrations\\examples\\CSV from SFTP Share\\station12_in_InfluxDB.txt",'r+')
+            uploaded_files = open(f"{DIR_PATH}\\{station_name}_in_InfluxDB.txt",'r+', encoding='utf-8')
+            text = uploaded_files.read().splitlines()
+            if csv_name in text:
+                NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                print(f"{str(NOW)}: Upload  of {csv_name} is already done. Skip this csv.")
+                logfile.write(f"{str(NOW)}: Upload  of {csv_name} is already done. Skip this csv.\n")
+                return
+        except FileNotFoundError as EXCPTN:
+            NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(f"{str(NOW)}: Error while reading file {EXCPTN}")
+            logfile.write(f"{str(NOW)}: Error while reading file {EXCPTN}\n")
             return
 
         # drop negative timestamps
         #data = data[data.index.year >= int(year)]
 
-        for column in data.columns:
-            data[column] = pd.to_numeric(data[column], errors="coerce")
-            data[column] = data[column].astype(float)
+        # for column in data.columns:
+        #     data[column] = pd.to_numeric(data[column], errors="coerce")
+        #     data[column] = data[column].astype(float)
 
         if len(data) < 1 or len(data.columns) < 1:
-            print("Receive empty csv. Skip that day.")
+            NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(f"{str(NOW)}: Receive empty csv. Skip that day.")
+            logfile.write(f"{str(NOW)}: Receive empty csv. Skip that day.\n")
             return
 
-        _measurment = DESCRIPTION_MAPPING[data['Description22'][0]]
-        data['value'] = data['Value22'] * data['Scale22']
-        data['meterID'] = data['DeviceId']
-        del data['Scale22']
-        del data['Description22']
-        del data['Unit22']
-        del data['Value22']
+        print(data.head())
+        print(data['DeviceId'])
+        #print(data['Value0'][1])
+        #row = 1
+        #while row <= len(data):
+        #    print(data['DeviceId'][row])
+        #    row = row + 1
+        #data2 = data
+        #del data2.df['Value0']
+        #print(data)
+        #print(data2)
+        
+        d = {'col1': [1, 2], 'col2': [3, 4]}
+        data_to_store = pd.DataFrame(data=d)
+        #data_to_store = data_to_store.fillna(0)
+        print(data_to_store)
+        
+        #_measurment = DESCRIPTION_MAPPING[data['Description7'][0]]
+        #_measurment = data['Description7'][0]
+        #data['value'] = data['Value7'] * data['Scale7']
+        #data_to_store['value'] = data['Value7'] * data['Scale7']
+        #data['unit'] = data['Unit7']
+        #data_to_store['unit'] = data['Unit7']
+        #data['meterID'] = data['DeviceId']
+        #data_to_store['meterID'] = data['DeviceId']
+        #del data['col1']
+        #del data['col2']
+        #del data['Value7']
+        #del data['Unit7']
+        #del data['Scale7']
+        #del data['Description7']
+        #del data['DeviceId']
+
+        _measurment = DESCRIPTION_MAPPING[data['Description0'][0]]
+        #_measurment = data['Description2']
+        data['value'] = data['Value0'] * data['Scale0']
+        data['unit'] = data['Unit0']
+        data['meterID'] = data['DeviceId']       
+        del data['Value0']
+        del data['Unit0']
+        del data['Scale0']
+        del data['Description0']
         del data['DeviceId']
+       
+        
 
         print(data)
+        #print(data_to_store)
         client.write_a_dataframe(
             project=TARGET_BUCKET,
             measurement_name=_measurment,
             dataframe=data,
+            #dataframe=data_to_store,
             tag_columns=['meterID'],
             additional_tags={'station': station_name}
         )
-
-        print(f"Finish migration. Write {len(data)} points to db.")
+        NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"{str(NOW)}: Finish migration. Write {len(data)} points to db.")
+        logfile.write(f"{str(NOW)}: Finish migration. Write {len(data)} points to db.\n")
 
 
         if len(data) < 1 or len(data.columns) < 1:
-            print("Receive empty csv. Skip that day.")
+            NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(f"{str(NOW)}: Receive empty csv. Skip that day.")
+            logfile.write(f"{str(NOW)}: Receive empty csv. Skip that day.\n")
             return
 
-        logfile.write(f"\n{csv_name}") 
-        logfile.close()
-        print(f"Finish migration. Write {len(data)} points to db.")
+        uploaded_files.write(f"{csv_name}\n") 
+        uploaded_files.close()
+        #NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        #print(f"{str(NOW)}: Finish migration. Write {len(data)} points to db.")
 
 
 # **********************************************************
 #   main function
 # **********************************************************
 
-station_folders = my_fs.listdir(f"{share}")
+try:
+    logfile = open(f"{DIR_PATH}\\{LOG_FILE}.log",'r+', encoding='utf-8')
+    #station_name = os.path.__file__    
+    total_lines = sum(1 for line in logfile)
+    NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(f"{str(NOW)}: Opening logfile {logfile.name}")
+    logfile.write(f"{str(NOW)}: Opening logfile {logfile.name}\n") 
 
-for station in station_folders:
-    if station in IGNORE_FILE:
-        continue
-    match = list(re.finditer(RE_STATION_ID, station))[0]
-    if match:
-        sta_name = match.group(1)
-        sta_id = match.group(2)
-        path_to_csvs = f"{share}/{sta_name}/{RE_CSV}"
+    station_folders = my_fs.listdir(f"{share}")
+
+    for station in station_folders:
+        if station in IGNORE_FILE:
+            continue
+        match = RE_STATION_ID.match(station)
+        if match is None:
+            continue
+
+        #sta_id = match.group(1)
+        path_to_csvs = f"{share}/{station}"
+        NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"{str(NOW)}: Scanning {station} for files")
+        logfile.write(f"{str(NOW)}: Scanning {station} for files\n")
 
         csvs = my_fs.listdir(path_to_csvs)
+        NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"{str(NOW)}: Sorting csvs in folder {station} started")
+        logfile.write(f"{str(NOW)}: Sorting csvs in folder {station} started\n")
+        csvs.sort()
+        NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"{str(NOW)}: Sorting csvs in folder {station} finished")
+        logfile.write(f"{str(NOW)}: Sorting csvs in folder {station} finished\n")
 
-        for csv in csvs:
-            matches = list(re.finditer(RE_CSV, csv)) 
-            csv_name = csv           
-            if len(matches) < 1:
-                continue
-            if matches[0]:
-                day = matches[0].group(1)
-                #if day_exists(year, gw_number, day):
-                    #continue
-                print(
-                    f"Start to migrate CSV from station {sta_name} from the {day}."
-                )
+        for csv in reversed(csvs):
+            match = RE_CSV.match(csv)
+            if match is None:
+                continue            
+            
+            time_in_unix = match.group(2)
+            time_in_UTC = DATE_PARSER(time_in_unix)
+            #if day_exists(year, gw_number, day):
+                #continue
+            NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(f"{str(NOW)}: Start to migrate CSV from {station} from {time_in_UTC} (UNIX: {time_in_unix}).")
+            logfile.write(f"{str(NOW)}: Start to migrate CSV from {station} from {time_in_UTC} (UNIX: {time_in_unix}).\n")
+            try:
+                migrate_csv(f'{path_to_csvs}/{csv}', station, csv)
+            except Exception:
+                # the migration failed first time. Wait a minute and try again ...
+                NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                print(f"{str(NOW)}: Migration failed the first time. Wait a minute and try again ...")
+                logfile.write(f"{str(NOW)}: Migration failed the first time. Wait a minute and try again ...\n")
+                sleep(60)
                 try:
-                    migrate_csv(path_to_csvs, sta_name, csv_name)
+                    migrate_csv(f'{path_to_csvs}/{csv}', station, csv)
                 except Exception:
-                    # the migration failed first time. Wait a minute and try again ...
-                    sleep(0.1)
-                    migrate_csv(path_to_csvs, sta_name, csv_name)
-        print(f"Measurment migration for the station {sta_name} completed.")
-print(f"Measurment migration for all stations completed.")
+                    NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    print(f"{str(NOW)}: Migration failed the second time. Skip this file.")
+                    logfile.write(f"{str(NOW)}: Migration failed the second time. Skip this file.\n")
+                    continue
+
+        NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"{str(NOW)}: Measurment migration for the station {station} completed.")
+        logfile.write(f"{str(NOW)}: Measurment migration for the station {station} completed.\n")
+    print(f"{str(NOW)}: Measurment migration for all stations completed.")
+    logfile.write(f"{str(NOW)}: Measurment migration for all stations completed.\n")
+
+    logfile.close
+except FileNotFoundError as EXCPTN:
+    NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(f"{str(NOW)}: Error while reading file {EXCPTN}")
+    logfile.write(f"{str(NOW)}: Error while reading file {EXCPTN}\n")
+    
+
+    
 
